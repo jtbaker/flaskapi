b4b7315098bd18b0a9e289e4315645563cc86a33
Jason Baker <jason.th.baker@gmail.com>
2017-11-23 01:42:18 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!flaskrest/bin/python3.6
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ secondfunction.py
@@ -0,0 +1,10 @@
+#!flaskrest/bin/python3.6
+
+from flask import abort, jsonify
+
+@app.route('/flaskrest/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
